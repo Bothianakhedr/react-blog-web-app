5be8daa8528a7658f20c7e98cdd82057ce4ed52c
Bothianakhedr <Bothianakhedr198@gmail.com>
2025-11-22 20:30:49 +0200

    fix problem

--- src/pages/Home/LatestPosts.tsx
+++ src/pages/Home/LatestPosts.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Skeleton from "./Components/Skeleton";
 import { PostCard } from "./Components/PostCard";
 import type {  PostType } from "./HomeTypes";
-import { getAllPosts } from "../../services/postServices";
+// import { getAllPosts } from "../../services/postServices";
 export const LatestPosts = () => {
   const [posts, setPosts] = useState<PostType[]>([]);
  
   // }
-  useEffect(() => {
-    getAllPosts({setPosts});
-  }, []);
+  // useEffect(() => {
+  //   getAllPosts({setPosts});
+  // }, []);
   return (
     <div className="post-list flex-9 mx-3 lg:mx-0 mt-9 grid gap-5 md:grid-cols-2 ">
       {posts.length ? (
